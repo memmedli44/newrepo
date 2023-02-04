520015f00e9e21394df8907bddbde198fef93427
mammadnm <mammadnm@CATR4>
2023-02-04 13:58:20 +0400

    iuyjno

---
+++ human age/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace human_age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //age:
+
+            // Console.WriteLine("age");
+            // int age =int.Parse(Console.ReadLine());
+
+
+
+            // if (1 <= age && age <= 1888) 
+            // {
+            //     int year = age / 12;
+            //     int month = age % 12;
+            //     Console.WriteLine($"{year} il {month} ay");
+
+            // }
+
+            // else
+            // {
+            //     Console.WriteLine("eded verilen araliqa uygun deyil");
+
+            //     goto age;
+
+            Console.WriteLine("hello");
+
+                //hello  1233444
+                co
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
